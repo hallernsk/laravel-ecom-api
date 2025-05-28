a9a2c6134a434c34ad8c76b003fef28b8e60b0cf
Vladimir <avladim@mail.ru>
2025-05-28 13:31:58 +0700

    created api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,36 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CartController;
+use App\Http\Controllers\API\OrderController;
+use App\Http\Controllers\API\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Product routes (public)
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+    
+    // Cart
+    Route::get('/cart', [CartController::class, 'getCart']);
+    Route::post('/cart/add', [CartController::class, 'addItem']);
+    Route::delete('/cart/remove', [CartController::class, 'removeItem']);
+    
+    // Orders
+    Route::post('/checkout', [OrderController::class, 'checkout']);
+    Route::get('/orders', [OrderController::class, 'index']);
+    Route::get('/orders/{id}', [OrderController::class, 'show']);
+});
+
+// Payment callback (public)
+Route::post('/orders/{id}/confirm-payment/{token}', [OrderController::class, 'confirmPayment']);
+
+
