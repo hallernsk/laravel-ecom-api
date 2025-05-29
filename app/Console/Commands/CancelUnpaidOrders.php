4a539650b5185008629f01061f8c0904fe40bf47
Vladimir <avladim@mail.ru>
2025-05-29 13:17:46 +0700

    created background update of orders status

---
+++ app/Console/Commands/CancelUnpaidOrders.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Order;
+use Illuminate\Console\Command;
+
+class CancelUnpaidOrders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    // protected $signature = 'app:cancel-unpaid-orders-command';
+    protected $signature = 'orders:cancel-unpaid';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Cancel unpaid orders older than 2 minutes';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $count = Order::where('status', 'pending')
+            ->where('created_at', '<', now()->subMinutes(2))
+            ->update(['status' => 'cancelled']);
+
+        $this->info("Cancelled orders: {$count}");
+
+    }
+}
